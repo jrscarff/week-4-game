2106769d38bee66dbb2c5c51cedbeb52cbef4d6e
Ryan Scarff <williamscarff@airbears2-10-142-154-38.airbears2.1918.berkeley.edu>
2018-01-23 11:10:19 -0800

    html file set up

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	function gameStart (){
+		var randomNumber = (Math.floor(Math.random() * 101) + 19);
+		console.log(randomNumber);
+		var gem1Number = Math.floor(Math.random() * 11) + 1;
+		var gem2Number = Math.floor(Math.random() * 11) + 1;
+		var gem3Number = Math.floor(Math.random() * 11) + 1;
+		var gem4Number = Math.floor(Math.random() * 11) + 1;
+		$("button").on("click", function () {
+			
+
+		})
+
+	}
+
+
+	gameStart();
+})
